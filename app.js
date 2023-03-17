fa7d4f6fb82a11b5a80a043a282402fd01ef3185
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-03-17 09:51:36 +0100

    Tested async-await pattern.

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
-const http = require("http");
+const { readFile, writeFile } = require("fs").promises; // PROMISES!
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Home Page");
-    console.log("home page");
-  } else if (req.url === "/about") {
-    // a crappy blocking code
-    for (let i = 0; i <= 100; i++) {
-      for (let j = 0; j <= 100; j++) {
-        console.log(`${i} - ${j}`);
-      }
-    }
-    res.end("About Page");
-    console.log("about page");
-  } else {
-    res.end("Error Page");
-    console.log("error page");
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result.txt",
+      `THIS IS AWESOME: ${first} ${second}`,
+      { flag: "a" }
+    );
+
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
   }
-});
+};
 
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000 ...");
-});
+start();
+
+// const start = async () => {
+//   try {
+//     const first = await readFilePromise("./content/first.txt", "utf8");
+//     const second = await readFilePromise("./content/second.txt", "utf8");
+//     await writeFilePromise(
+//       "./content/result.txt",
+//       `THIS IS AWESOME: ${first} ${second}`,
+//       { flag: "a" }
+//     );
+//     console.log(first, second);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("../content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
