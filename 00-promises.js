25112130cac89f940b3101e1e22647e61335f4f6
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-03-18 15:10:27 +0100

    Completed promises part.

--- 00-promises.js
+++ 00-promises.js
@@ -21,8 +21,45 @@ let order = (time, work) => {
 
 order(2000, () => {
   console.log(`${stocks.fruits[0]} was selected`);
-}).then(() => {
-  return order(0000, () => {
-    console.log("production has started");
+})
+  .then(() => {
+    return order(0000, () => {
+      console.log("production has started");
+    });
+  })
+  .then(() => {
+    return order(2000, () => {
+      console.log("the fruits was chopped");
+    });
+  })
+  .then(() => {
+    return order(1000, () => {
+      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`);
+    });
+  })
+  .then(() => {
+    return order(1000, () => {
+      console.log("machine has started");
+    });
+  })
+  .then(() => {
+    return order(2000, () => {
+      console.log(`ice cream was pleased on ${stocks.holder[0]}`);
+    });
+  })
+  .then(() => {
+    return order(3000, () => {
+      console.log(`${stocks.toppings[0]} was selected`);
+    });
+  })
+  .then(() => {
+    return order(1000, () => {
+      console.log("ice cream was served");
+    });
+  })
+  .catch(() => {
+    console.log("customer left");
+  })
+  .finally(() => {
+    console.log("DAY ENDED, shop is closed");
   });
-});
