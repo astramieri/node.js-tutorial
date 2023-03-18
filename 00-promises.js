21397c129b4c2fde4877352f2965ede3a6ec6fa8
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-03-18 07:16:52 +0100

    Tested promises chain.

--- 00-promises.js
+++ 00-promises.js
@@ -1,1 +1,28 @@
-//TOFIX
+let stocks = {
+  fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "sticks"],
+  toppings: ["chocolate", "peanuts"],
+};
+
+let isShopOpen = true;
+
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (isShopOpen) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log("Our shop is closed!"));
+    }
+  });
+};
+
+order(2000, () => {
+  console.log(`${stocks.fruits[0]} was selected`);
+}).then(() => {
+  return order(0000, () => {
+    console.log("production has started");
+  });
+});
