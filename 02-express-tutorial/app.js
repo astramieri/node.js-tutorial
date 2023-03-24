0a8ca99f9f50ef0abf2237b8d93271d2760f299e
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-03-24 06:51:54 +0100

    Back to http node module.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,12 @@
-console.log("Express Tutorial");
+const http = require("http");
+
+const server = http.createServer(
+  // this callback is invoked everytime the user hits our server
+  (req, res) => {
+    console.log("user hit the server");
+
+    res.end("home page"); // must be called on each respond
+  }
+);
+
+server.listen(3000);
