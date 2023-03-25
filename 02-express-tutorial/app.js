213d000933060a63c246a9fcfb2b3f1d0b1f96de
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-03-25 15:09:02 +0100

    Modified few lines of code.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,10 +3,22 @@ const http = require("http");
 const server = http.createServer(
   // this callback is invoked everytime the user hits our server
   (req, res) => {
-    console.log("user hit the server");
-    res.writeHead(200, {'content-type' : 'text/plain'});
-    res.write("<h1>okay<h1>");
-    res.end(); // must be called on each respond
+    const url = req.url;
+    if (url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>home page<h1>");
+      res.end(); // must be called on each respond
+    } else if (url === "/about") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>about page<h1>");
+      res.end(); // must be called on each respond
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>page not found<h1>");
+      res.end(); // must be called on each respond
+    }
+
+    console.log(`${req.method} : ${req.url}`);
   }
 );
 
