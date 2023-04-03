cccb3aba6e763b7caa07d58954c1556493294347
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-03 07:17:58 +0200

    First JSON response.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,8 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-// setup static and middleware
-app.use(express.static("./public")); // tip: use PUBLIC or STATIC
-
-// app.get("/", (req, res) => {
-//   // ALTERNATIVES:
-//   // 1) static files (./public)
-//   // 2) SSR (server side rendering)
-//   res.status(200).sendFile(path.resolve(__dirname, "./resource/index.html"));
-// });
-
-app.get("*", (req, res) => {
-  res.status(404).send("Resource not found");
+app.get("/", (req, res) => {
+  res.json([{ name: "Peter" }, { name: "John" }]);
 });
 
 app.listen(3000, () => {
