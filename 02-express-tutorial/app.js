665cbaf04036cc3f37cf3b31bcf23448824444bb
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-10 09:15:13 +0200

    Post methods on the go.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,38 +1,22 @@
 const express = require("express");
-const morgan = require("morgan");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
+let data = require("./resource/data");
 
-// request => middleware => response
+// static assets
+app.use(express.static("./methods"));
+// parse data
+app.use(express.urlencoded({extended:false}));
 
-// MIDDLEWARE
-// option 1: specify in every route
-// option 1: set a default with app.use
-
-// WARN: app.use invokes logger function fon any route
-// WARN: the order is important (here will be used for every GET)
-
-//app.use(logger); // every path
-//app.use('/api', logger); // every path AFTER /api
-//app.use([authorize, logger]); //multiple use: executed in this order
-app.use(morgan("common"));
-
-app.get("/", (req, res) => {
-  res.send("Home page");
+app.get("/api/people", (req, res) => {
+  res.status(200).json(data.people);
 });
 
-app.get("/about", (req, res) => {
-  res.send("About page");
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.send("Login");
 });
+  
 
-app.get("/api/products", (req, res) => {
-  res.send("Products page");
-});
-
-app.get("/api/items", [authorize, logger], (req, res) => {
-  res.send("Items page");
-});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000 ...");
---
+++ 02-express-tutorial/scripts/06-middleware-use.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const logger = require("./logger");
+const authorize = require("./authorize");
+
+// request => middleware => response
+
+// MIDDLEWARE
+// option 1: specify in every route
+// option 1: set a default with app.use
+
+// WARN: app.use invokes logger function fon any route
+// WARN: the order is important (here will be used for every GET)
+
+//app.use(logger); // every path
+//app.use('/api', logger); // every path AFTER /api
+//app.use([authorize, logger]); //multiple use: executed in this order
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products page");
+});
+
+app.get("/api/items", [authorize, logger], (req, res) => {
+  res.send("Items page");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000 ...");
+});
