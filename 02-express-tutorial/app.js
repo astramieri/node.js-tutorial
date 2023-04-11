7f5324df1c412a39fe8eb5110bd71da39ea2cca5
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-11 07:00:05 +0200

    Done the traditional approach to send data.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,8 +4,8 @@ let data = require("./resource/data");
 
 // static assets
 app.use(express.static("./methods"));
-// parse data
-app.use(express.urlencoded({extended:false}));
+// parse data (this is crucial in order to get data)
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/people", (req, res) => {
   res.status(200).json(data.people);
@@ -13,10 +13,12 @@ app.get("/api/people", (req, res) => {
 
 app.post("/login", (req, res) => {
   console.log(req.body);
-  res.send("Login");
+  const { firstname } = req.body; // de-structuring
+  if (firstname) {
+    return res.status(200).send(`Welcome ${firstname}`);
+  }
+  res.status(401).send("Please provide credentials");
 });
-  
-
 
 app.listen(3000, () => {
   console.log("Listening on port 3000 ...");
